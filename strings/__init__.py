80bfa207c89f7e6db766558d5ddb5d9ccce1aeca
HMD7SLK <sajd.haka11@gmail.com>
2026-06-23 13:14:26 +0200

    Update __init__.py

--- strings/__init__.py
+++ strings/__init__.py
@@ -1,6 +1,4 @@
 import os
-from typing import List
-
 import yaml
 
 languages = {}
@@ -11,24 +9,31 @@ def get_string(lang: str):
     return languages[lang]
 
 
-for filename in os.listdir(r"./strings/langs/"):
-    if "en" not in languages:
-        languages["en"] = yaml.safe_load(
-            open(r"./strings/langs/en.yml", encoding="utf8")
-        )
-        languages_present["en"] = languages["en"]["name"]
-    if filename.endswith(".yml"):
-        language_name = filename[:-4]
-        if language_name == "en":
-            continue
-        languages[language_name] = yaml.safe_load(
-            open(r"./strings/langs/" + filename, encoding="utf8")
-        )
-        for item in languages["en"]:
-            if item not in languages[language_name]:
-                languages[language_name][item] = languages["en"][item]
+for filename in os.listdir("./strings/langs/"):
     try:
-        languages_present[language_name] = languages[language_name]["name"]
-    except:
-        print("There is some issue with the language file inside bot.")
+        if "en" not in languages:
+            languages["en"] = yaml.safe_load(
+                open("./strings/langs/en.yml", encoding="utf8")
+            )
+            languages_present["en"] = languages["en"]["name"]
+
+        if filename.endswith(".yml"):
+            language_name = filename[:-4]
+
+            if language_name == "en":
+                continue
+
+            languages[language_name] = yaml.safe_load(
+                open(f"./strings/langs/{filename}", encoding="utf8")
+            )
+
+            for item in languages["en"]:
+                if item not in languages[language_name]:
+                    languages[language_name][item] = languages["en"][item]
+
+            languages_present[language_name] = languages[language_name]["name"]
+
+    except Exception as e:
+        print(f"Language Error in file: {filename}")
+        print(f"Error: {e}")
         exit()
