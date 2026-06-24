4122f50b0b7ace6b15d1e7aba58f18bebd47d63e
HMD7SLK <sajd.haka11@gmail.com>
2026-06-25 01:26:25 +0200

    Refactor logging and message formatting in bot.py

--- YousefMusic/core/bot.py
+++ YousefMusic/core/bot.py
@@ -1,15 +1,12 @@
 from pyrogram import Client, errors
-from pyrogram.enums import ChatMemberStatus, ParseMode
 
 import config
-
 from ..logging import LOGGER
 
-# 𝐃𝐞𝐩𝐥𝐨𝐲𝐞𝐝 ⛥ 𓏺 Yousef .tele_https://t.me/y_o_v
 
 class Zoro(Client):
     def __init__(self):
-        LOGGER("ميــوزك بحر").info(f"جارِ بدء تشغيل البوت . . .")
+        LOGGER("ميــوزك بحر").info("جارِ بدء تشغيل البوت . . .")
         super().__init__(
             name="YousefMusic",
             api_id=config.API_ID,
@@ -19,28 +16,49 @@ class Zoro(Client):
 
     async def start(self):
         await super().start()
+
         self.id = self.me.id
         self.name = self.me.first_name + " " + (self.me.last_name or "")
         self.username = self.me.username
         self.mention = self.me.mention
 
         try:
+            print(f"LOGGER_ID = {config.LOGGER_ID}")
+
+            chat = await self.get_chat(config.LOGGER_ID)
+            print(f"LOGGER CHAT = {chat.title}")
+
             await self.send_message(
                 chat_id=config.LOGGER_ID,
-                text=f"<u><b>» تم تشغيل الميـوزك لـ البوت {self.mention} :</b><u>\n\n- ɪᴅ : <code>{self.id}</code>\n- ɴᴀᴍᴇ : {self.name}\n- ᴜsᴇʀɴᴀᴍᴇ : @{self.username}",
+                text=(
+                    f"» تم تشغيل الميوزك\n\n"
+                    f"ID : {self.id}\n"
+                    f"NAME : {self.name}\n"
+                    f"USERNAME : @{self.username}"
+                ),
             )
-        except (errors.ChannelInvalid, errors.PeerIdInvalid):
-            LOGGER(__name__).error(
-                "» قم باضافة البـوت مشـرفـاً بكافة الصلاحيات في مجموعـة السجـل"
-            )
-            exit()
-        except Exception as ex:
-            LOGGER(__name__).error(
-                f"Bot has failed to access the log group/channel.\n  Reason : {type(ex).__name__}."
-            )
-            exit()
 
-        LOGGER("ميــوزك بحر").info(f" تم بدء تشغيل البوت {self.name} ...✓")
+        except (errors.ChannelInvalid, errors.PeerIdInvalid) as ex:
+            print(f"LOGGER ERROR => {ex}")
+            LOGGER(__name__).error(
+                f"ChannelInvalid / PeerIdInvalid : {ex}"
+            )
+            raise ex
+
+        except Exception as ex:
+            import traceback
+
+            print(f"LOGGER ERROR => {ex}")
+            traceback.print_exc()
+
+            LOGGER(__name__).error(
+                f"Bot has failed to access the log group/channel.\nReason: {ex}"
+            )
+            raise ex
+
+        LOGGER("ميــوزك بحر").info(
+            f"تم بدء تشغيل البوت {self.name} ...✓"
+        )
 
     async def stop(self):
         await super().stop()
