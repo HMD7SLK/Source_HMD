28ff05a7fd026a667c0ad2298c06bdad87e4c80f
HMD7SLK <sajd.haka11@gmail.com>
2026-06-24 22:19:28 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+from threading import Thread
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"Bot is running")
+
+def run_web():
+    port = int(os.environ.get("PORT", 10000))
+    server = HTTPServer(("0.0.0.0", port), Handler)
+    server.serve_forever()
+
+Thread(target=run_web).start()
