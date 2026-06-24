8c6272bab0354246f8a787e0be2839fb0eac2768
HMD7SLK <sajd.haka11@gmail.com>
2026-06-24 23:47:22 +0200

    Refactor main.py to use asyncio and improve logging

--- main.py
+++ main.py
@@ -1,22 +1,72 @@
-import os
-from threading import Thread
-from http.server import BaseHTTPRequestHandler, HTTPServer
+import web
+
+import asyncio
+import importlib
+import logging
+import traceback
+
+from pyrogram import idle
+
+import config
+from YousefMusic import LOGGER, app, userbot
+from YousefMusic.core.call import Zoro
+from YousefMusic.misc import sudo
+from YousefMusic.plugins import ALL_MODULES
+from YousefMusic.utils.database import get_banned_users, get_gbanned
+from config import BANNED_USERS
+
+logging.basicConfig(level=logging.INFO)
 
 
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b"Bot is running")
+async def init():
 
-    def log_message(self, format, *args):
-        return  # يمنع logs مزعجة
+    if (
+        not config.STRING1
+        and not config.STRING2
+        and not config.STRING3
+        and not config.STRING4
+        and not config.STRING5
+    ):
+        LOGGER(__name__).error("كود جلسة الحساب المساعد غير موجود")
+        return
 
+    await sudo()
 
-def run_web():
-    port = int(os.environ.get("PORT", 10000))
-    server = HTTPServer(("0.0.0.0", port), Handler)
-    server.serve_forever()
+    try:
+        users = await get_gbanned()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
 
+        users = await get_banned_users()
+        for user_id in users:
+            BANNED_USERS.add(user_id)
 
-Thread(target=run_web).start()
+    except Exception as e:
+        print("DB ERROR:", e)
+        traceback.print_exc()
+
+    await app.start()
+
+    for all_module in ALL_MODULES:
+        importlib.import_module("YousefMusic.plugins" + all_module)
+
+    LOGGER("YousefMusic").info("تم تحميل الإضافات ✓")
+
+    try:
+        await userbot.start()
+        await Zoro.start()
+        await Zoro.decorators()
+
+        LOGGER("YousefMusic").info("تم تشغيل البوت بنجاح ✓")
+
+    except Exception as e:
+        print("START ERROR:", e)
+        traceback.print_exc()
+
+    await idle()
+
+    LOGGER("YousefMusic").info("جاري إيقاف البوت...")
+
+    try:
+        await app.stop()
+        await
